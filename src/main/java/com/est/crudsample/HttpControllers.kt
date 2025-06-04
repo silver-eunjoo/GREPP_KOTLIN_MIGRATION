6bd5f5bc48f99f3b66171a3eb094fbdfd428990a
silver-eunjoo <leeeun5423@naver.com>
2025-06-04 11:03:08 +0900

    필요없는 import 제거

--- src/main/java/com/est/crudsample/HttpControllers.kt
+++ src/main/java/com/est/crudsample/HttpControllers.kt
@@ -1,7 +1,5 @@
 package com.est.crudsample
 
-import com.est.crudsample.dto.GeneralApiResponse
-import com.est.crudsample.dto.TaskDto
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
